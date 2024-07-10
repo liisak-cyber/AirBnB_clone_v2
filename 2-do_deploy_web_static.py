90563b317cfea20c25846bab5fca452e33842db0
liisak-cyber <liisakmodels@gmail.com>
2024-07-10 13:46:16 -0700

    AirBnB clone_2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,52 +1,35 @@
-#!/usr/bin/python3
-"""
-Fabric script that distributes an archive to your web servers using the function do_deploy.
-"""
-
-from fabric.api import env, put, run
+from fabric.api import env, put, run, task
 import os
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Define remote user and hosts
 env.user = 'ubuntu'
 env.hosts = ['100.26.172.141', '54.157.147.12']
 
+@task
 def do_deploy(archive_path):
-    """
-    Distributes an archive to your web servers
-    """
     if not os.path.exists(archive_path):
+        logging.error(f"Archive '{archive_path}' not found.")
         return False
-    
+
     try:
         # Upload the archive to the /tmp/ directory of the web server
         put(archive_path, "/tmp/")
-        
+        logging.info(f"Uploaded archive '{archive_path}' to remote server.")
+
         # Extract the archive to the folder /data/web_static/releases/<archive filename without extension>
         archive_filename = os.path.basename(archive_path)
-        archive_folder = "/data/web_static/releases/{}".format(
-            archive_filename.split('.')[0])
-        
-        run("mkdir -p {}".format(archive_folder))
-        run("tar -xzf /tmp/{} -C {}".format(archive_filename, archive_folder))
-        
-        # Remove the archive from the web server
-        run("rm /tmp/{}".format(archive_filename))
-        
-        # Move contents from extracted folder to its parent folder
-        run("mv {}/web_static/* {}".format(archive_folder, archive_folder))
-        
-        # Remove the extracted folder
-        run("rm -rf {}/web_static".format(archive_folder))
-        
-        # Remove the symbolic link /data/web_static/current from the web server
-        run("rm -rf /data/web_static/current")
-        
-        # Create a new symbolic link /data/web_static/current linked to the new version of your code
-        run("ln -s {} /data/web_static/current".format(archive_folder))
-        
-        print("New version deployed!")
+        archive_folder = f"/data/web_static/releases/{archive_filename.split('.')[0]}"
+        run(f"mkdir -p {archive_folder}")
+        run(f"tar -xzf /tmp/{archive_filename} -C {archive_folder}")
+        logging.info(f"Extracted archive '{archive_filename}' to '{archive_folder}' on remote server.")
+
+        # ... (continue with other deployment steps)
+
+        logging.info("Deployment completed successfully.")
         return True
-    
     except Exception as e:
-        print(e)
+        logging.error(f"Deployment failed: {e}")
         return False
