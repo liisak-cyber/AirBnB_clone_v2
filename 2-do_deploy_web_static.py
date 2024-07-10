bc1eb4c5472b86ee47c3ab8d71b8ba3cd1e6a22c
liisak-cyber <liisakmodels@gmail.com>
2024-07-09 18:46:06 -0700

    2-do_deploy_web_static.py

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+Fabric script that distributes an archive to your web servers using the function do_deploy.
+"""
+
+from fabric.api import env, put, run
+import os
+
+# Define remote user and hosts
+env.user = 'ubuntu'
+env.hosts = ['<IP web-01>', '<IP web-02>']
+
+def do_deploy(archive_path):
+    """
+    Distributes an archive to your web servers
+    """
+    if not os.path.exists(archive_path):
+        return False
+    
+    try:
+        # Upload the archive to the /tmp/ directory of the web server
+        put(archive_path, "/tmp/")
+        
+        # Extract the archive to the folder /data/web_static/releases/<archive filename without extension>
+        archive_filename = os.path.basename(archive_path)
+        archive_folder = "/data/web_static/releases/{}".format(
+            archive_filename.split('.')[0])
+        
+        run("mkdir -p {}".format(archive_folder))
+        run("tar -xzf /tmp/{} -C {}".format(archive_filename, archive_folder))
+        
+        # Remove the archive from the web server
+        run("rm /tmp/{}".format(archive_filename))
+        
+        # Move contents from extracted folder to its parent folder
+        run("mv {}/web_static/* {}".format(archive_folder, archive_folder))
+        
+        # Remove the extracted folder
+        run("rm -rf {}/web_static".format(archive_folder))
+        
+        # Remove the symbolic link /data/web_static/current from the web server
+        run("rm -rf /data/web_static/current")
+        
+        # Create a new symbolic link /data/web_static/current linked to the new version of your code
+        run("ln -s {} /data/web_static/current".format(archive_folder))
+        
+        print("New version deployed!")
+        return True
+    
+    except Exception as e:
+        print(e)
+        return False
