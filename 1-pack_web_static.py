97ebb6419e7cab78e4ceca3c1e1a77d6c7f62ade
liisak-cyber <liisakmodels@gmail.com>
2024-07-09 17:37:34 -0700

    1-pack_web_static

---
+++ 1-pack_web_static.py
@@ -0,0 +1,31 @@
+from fabric.api import local
+from datetime import datetime
+import os
+
+def do_pack():
+    """Creates a .tgz archive from the contents of the web_static folder."""
+    try:
+        # Create versions directory if it doesn't exist
+        if not os.path.exists("versions"):
+            local("mkdir versions")
+
+        # Generate current time in UTC
+        now = datetime.utcnow()
+
+        # Format the archive name web_static_<year><month><day><hour><minute><second>.tgz
+        archive_name = "web_static_{}{}{}{}{}{}.tgz".format(
+            now.year, now.month, now.day, now.hour, now.minute, now.second)
+
+        # Create the tar command to compress the web_static folder
+        command = "tar -cvzf versions/{} web_static".format(archive_name)
+
+        # Execute the command locally
+        local(command)
+
+        # Return the path to the created archive
+        return "versions/{}".format(archive_name)
+
+    except Exception as e:
+        print(e)
+        return None
+
